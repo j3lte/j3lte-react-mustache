557c390964a99ec1ed75f818c2651e71223f1eaa
Jelte Lagendijk <j3lte@users.noreply.github.com>
2024-05-14 11:03:18 +0200

    Update Mustache.jsx
    Wrap the fetch() in a React.useEffect() and conditionally render the
    component only once the template is defined.

--- src/routes/Mustache.jsx
+++ src/routes/Mustache.jsx
@@ -7,20 +7,27 @@ const data = { name: 'Fred' };
 function Mustache() {
   let [text, setText] = React.useState();
 
-  // Loading the template as a file DOES NOT work:
-  fetch(textfile)
-    .then((response) => response.text())
-    .then((textContent) => {
-      setText(textContent);
-    });
+  // @j3lte: You should wrap fetch in a useEffect
+  React.useEffect(() => {
+    // Loading the template as a string DOES work:
+    fetch(textfile)
+      .then((response) => response.text())
+      .then((textContent) => {
+        setText(textContent);
+      });
+  }, []);
 
   // Using a text string as the template works:
   //text = "<p>Lorem ipsum <strong>{{name}}</strong> amet consectetur adipisicing elit.";
 
+  // @j3lte: Conditionally render the component, only when template is defined...
   return (
     <>
       <h1>Mustache</h1>
+      { text ? 
       <TemplateComponent template={text} data={data} />
+      : <p>Loading...</p>
+      }
     </>
   );
 }
